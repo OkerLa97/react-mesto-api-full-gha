54884c26a70b10b68d886fc3666b4e06736bdcf2
Oker <oker97@mail.ru>
2023-05-31 20:30:21 +0200

    cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -3,12 +3,12 @@ module.exports = (req, res, next) => {
   const { method } = req; // Записываем в переменную method соответствующий заголовок
 
   res.header('Access-Control-Allow-Origin', '*'); // Устанавливаем заголовок, который позволяет запросы с любого домена
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); // Устанавливаем заголовок, который разрешает браузеру отправлять дополнительные заголовки в запросе
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); // Добавляем заголовок "Authorization" в список разрешенных заголовков
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE'); // Устанавливаем заголовок, который разрешает браузеру отправлять дополнительные заголовки в запросе
 
   if (method === 'OPTIONS') {
     // разрешаем кросс-доменные запросы с этими заголовками
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     // отправляем HTTP OK статус для предварительного запроса
     return res.status(200).end();
   }
